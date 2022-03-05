4b05a607aa5661e2b55f284d0550f39d1142b746
richardky30 <93296008+richardky30@users.noreply.github.com>
2022-03-04 22:14:39 -0800

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,3 +1,9 @@
+/* 
+ * Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+ * Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+ * If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+ */
+
 public class Order {
   public static String order(String words) {
     String[] wordList = words.split(" ");
@@ -14,20 +20,16 @@ public class Order {
       String[] characters = word.split("");
       
       for (String character: characters) {
-        
         if (Character.isDigit(character.charAt(0))) 
           orderedWords[character.charAt(0) - '0' - 1] = word;
-      
-      }
-               
+      }        
     }
     
     for (int i = 0; i < orderedWords.length; i++) {
-      
       if (orderedWords[i] != null) 
         finalList = finalList + " " + orderedWords[i];
-      
     }
+    
     return finalList.trim();
   }
 }
